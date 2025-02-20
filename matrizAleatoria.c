a3b913a6a81aca883ae3166b854fb53d6409c6dc
Gabriel Coelho Góes <161400667+Cogah@users.noreply.github.com>
2025-02-20 13:15:51 -0300

    Update matrizAleatoria.c

--- matrizAleatoria.c
+++ matrizAleatoria.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]) {
 	srand(time(NULL));
 	int i,j;
 	int mat[4][5];
-	//laços para preencher a matriz
+	// Laços para preencher a matriz
 	for(i = 0; i < 4; i++){
 		for(j = 0; j < 5; j++){
 			mat[i][j] = rand()%10;
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	
-	//laços para escrever a matriz
+	// Laços para escrever a matriz
 	for(i = 0; i < 4; i++){
 		for(j = 0; j < 5; j++){
 			printf("[%d] ",mat[i][j]);
