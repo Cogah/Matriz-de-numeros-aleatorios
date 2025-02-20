563913c57de9dcbc0fa88b16e2c888b1e14f7c17
Gabriel Coelho Góes <161400667+Cogah@users.noreply.github.com>
2025-02-20 13:15:21 -0300

    Add files via upload

---
+++ matrizAleatoria.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	srand(time(NULL));
+	int i,j;
+	int mat[4][5];
+	//laços para preencher a matriz
+	for(i = 0; i < 4; i++){
+		for(j = 0; j < 5; j++){
+			mat[i][j] = rand()%10;
+			if(mat[i][j]%2 != 0){
+				j--;
+			}
+		}
+	}
+	
+	//laços para escrever a matriz
+	for(i = 0; i < 4; i++){
+		for(j = 0; j < 5; j++){
+			printf("[%d] ",mat[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
